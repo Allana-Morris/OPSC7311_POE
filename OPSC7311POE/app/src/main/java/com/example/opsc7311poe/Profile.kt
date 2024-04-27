700b42f62e28c20000dc24948960b56b18b3f0a0
ST10080623 <102595062+RMOC500@users.noreply.github.com>
2024-04-27 17:14:11 +0200

    Finished :) Final UI push

--- OPSC7311POE/app/src/main/java/com/example/opsc7311poe/Profile.kt
+++ OPSC7311POE/app/src/main/java/com/example/opsc7311poe/Profile.kt
@@ -1,36 +1,20 @@
 package com.example.opsc7311poe
 
 import android.os.Bundle
-import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class Profile : AppCompatActivity() {
-    private val navBar = Navbar()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val HomeOpenActivity = findViewById<TextView>(R.id.tv_Home)
-        val ProfileOpenActivity = findViewById<TextView>(R.id.tv_Profile)
-        val CalendarOpenActivity = findViewById<TextView>(R.id.tv_calendar)
-        val TimerOpenActivity = findViewById<TextView>(R.id.tv_timer)
-
-        HomeOpenActivity.setOnClickListener()
-        {
-            navBar.OpenHomeButton()
-        }
-
-        ProfileOpenActivity.setOnClickListener()
-        {
-            navBar.OpenProfileButton()
-        }
-
-        CalendarOpenActivity.setOnClickListener()
-        {
-            navBar.OpenCalendarButton()
-        }
-
-        TimerOpenActivity.setOnClickListener()
-        {
-            navBar.OpenTimerButton()
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_profile)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
 }
