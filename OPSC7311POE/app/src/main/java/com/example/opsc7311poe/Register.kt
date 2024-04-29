9b0e83a46a80a3763a3af80ca717213819928938
ST10030798 <ST10030798@vcconnect.edu.za>
2024-04-29 17:03:53 +0200

    Added user capture and user List addition. Also changed name of register activity activity_register.xml

--- OPSC7311POE/app/src/main/java/com/example/opsc7311poe/Register.kt
+++ OPSC7311POE/app/src/main/java/com/example/opsc7311poe/Register.kt
@@ -1,4 +1,42 @@
 package com.example.opsc7311poe
 
-class Register {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        val signUpButton: Button = findViewById(R.id.btnSignUp)
+        val loginButton: Button = findViewById(R.id.btnLogin)
+
+        signUpButton.setOnClickListener()
+        {
+            val username: EditText = findViewById(R.id.txtUserName)
+            val fullName: EditText = findViewById(R.id.txtFullName)
+            val email: EditText = findViewById(R.id.txtEmail)
+            val password: EditText = findViewById(R.id.txtPassword)
+
+            val user = User(username.text.toString(), fullName.text.toString(),
+                password.text.toString(), email.text.toString());
+
+            // val user = User("SexMaster", "Phil Collins", "12345", "hello@gmail.com");
+
+            UserList.users.add(user);
+
+            val message = "User signed up: ${user.username}"
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        }
+
+        loginButton.setOnClickListener {
+            // Create an Intent to navigate to ActivityLogin
+            val intent = Intent(this, Login ::class.java)
+            startActivity(intent)
+        }
+    }
 }
