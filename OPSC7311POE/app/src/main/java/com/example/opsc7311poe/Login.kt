9bb3a36fd2d415555b23f8f60cee92adbba49554
Allana-Morris <ST10204772@vcconnect.edu.za>
2024-04-30 11:13:55 +0200

    Login Authentication/Check if User exists

--- OPSC7311POE/app/src/main/java/com/example/opsc7311poe/Login.kt
+++ OPSC7311POE/app/src/main/java/com/example/opsc7311poe/Login.kt
@@ -1,20 +1,25 @@
 package com.example.opsc7311poe
 
-import android.annotation.SuppressLint
+
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class Login : AppCompatActivity() {
+    private lateinit var usernameTextView: TextView
+    private lateinit var passwordTextView: TextView
 
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+        usernameTextView = findViewById(R.id.tv_Username)
+        usernameTextView = findViewById(R.id.tv_Password)
+
         val buttonOpenActivity = findViewById<TextView>(R.id.tv_Open_Register)
         buttonOpenActivity.setOnClickListener {
             val intent = Intent(this, Register::class.java)
@@ -23,11 +28,18 @@ class Login : AppCompatActivity() {
         val buttonLogin = findViewById<Button>(R.id.btn_Login)
         buttonLogin.setOnClickListener {
             //Login code - Checks and Stuffs
-
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            val userName = usernameTextView.text.toString()
+            val password = passwordTextView.text.toString()
+            if (authenticate(userName, password)) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
-
+    private fun authenticate(username: String, password: String): Boolean {
+        return UserList.users.any { it.username == username && it.password == password }
+    }
 }
