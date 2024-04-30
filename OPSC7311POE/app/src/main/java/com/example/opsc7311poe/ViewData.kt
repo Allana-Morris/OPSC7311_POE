e200b7f618be26502048f1dbbd0a92a8f4e13914
Allana-Morris <ST10204772@vcconnect.edu.za>
2024-04-30 14:52:42 +0200

    Added Code for Navbar Navigation

--- OPSC7311POE/app/src/main/java/com/example/opsc7311poe/ViewData.kt
+++ OPSC7311POE/app/src/main/java/com/example/opsc7311poe/ViewData.kt
@@ -1,4 +1,36 @@
 package com.example.opsc7311poe
 
-class ViewData {
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class ViewData : AppCompatActivity() {
+    private val navBar = Navbar()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val HomeOpenActivity = findViewById<TextView>(R.id.tv_Home)
+        val ProfileOpenActivity = findViewById<TextView>(R.id.tv_Profile)
+        val CalendarOpenActivity = findViewById<TextView>(R.id.tv_calendar)
+        val TimerOpenActivity = findViewById<TextView>(R.id.tv_timer)
+
+        HomeOpenActivity.setOnClickListener()
+        {
+            navBar.OpenHomeButton()
+        }
+
+        ProfileOpenActivity.setOnClickListener()
+        {
+            navBar.OpenProfileButton()
+        }
+
+        CalendarOpenActivity.setOnClickListener()
+        {
+            navBar.OpenCalendarButton()
+        }
+
+        TimerOpenActivity.setOnClickListener()
+        {
+            navBar.OpenTimerButton()
+        }
+    }
 }
