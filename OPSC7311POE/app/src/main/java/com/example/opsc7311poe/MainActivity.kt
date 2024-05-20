ae766ef2fa5dc99673958fd0dae14f1fde564f5c
Allana-Morris <ST10204772@vcconnect.edu.za>
2024-05-20 08:42:19 +0200

    Added Firebase (connected to online Firebase on my personal account and added SDK. Database variable in MainActivity.kt - shows format to create variable when needed)

--- OPSC7311POE/app/src/main/java/com/example/opsc7311poe/MainActivity.kt
+++ OPSC7311POE/app/src/main/java/com/example/opsc7311poe/MainActivity.kt
@@ -11,9 +11,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 class MainActivity : AppCompatActivity() {
-
+    val AppDataBase = Firebase.firestore
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
