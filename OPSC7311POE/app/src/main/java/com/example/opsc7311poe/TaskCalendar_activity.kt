d6883f34e180ca2852b30590d2791332f88e22e5
Allana-Morris <ST10204772@vcconnect.edu.za>
2024-06-10 19:28:08 +0200

    Halfway with Calendar

--- OPSC7311POE/app/src/main/java/com/example/opsc7311poe/TaskCalendar_activity.kt
+++ OPSC7311POE/app/src/main/java/com/example/opsc7311poe/TaskCalendar_activity.kt
@@ -6,11 +6,20 @@ import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.ListView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import java.text.SimpleDateFormat
 import java.time.LocalDate
+import java.util.Calendar
+import java.util.Locale
 
 class TaskCalendar_activity : AppCompatActivity() {
     lateinit var set_event: TextView
@@ -21,6 +30,12 @@ class TaskCalendar_activity : AppCompatActivity() {
     var onScreenDate = datenow
     lateinit var bottomNav: BottomNavigationView
 
+    lateinit var  currentUserTasksRef: DatabaseReference
+    private val tasks = mutableListOf<Task>()
+    private val dateFormat = SimpleDateFormat("dd MM", Locale.getDefault())
+    private var currentDate = Calendar.getInstance()
+    val database = FirebaseDatabase.getInstance()
+    val reference = database.getReference("categories")
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -86,6 +101,47 @@ class TaskCalendar_activity : AppCompatActivity() {
                 }
             }
         }
+
+        //For Each loop for Categories
+        val database: FirebaseDatabase =
+            FirebaseDatabase.getInstance("https://atomic-affinity-421915-default-rtdb.europe-west1.firebasedatabase.app/")
+
+        //val DbRef = database.getReference("user")
+        val currentUsername = SessionUser.currentUser?.username
+        if (currentUsername != null) {
+            currentUserTasksRef = database.getReference("user").child(currentUsername).child("categories")
+        } else {
+            Toast.makeText(this, "Current user not found", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        // Add a ValueEventListener to fetch tasks from the database
+        currentUserTasksRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (categorySnapshot in dataSnapshot.children) {
+                    for (taskSnapshot in categorySnapshot.child("tasks").children) {
+                        val task = taskSnapshot.getValue(Task::class.java)
+                        task?.let {
+                            if (it.isRepeating) {
+                                tasks.add(it)
+                            }
+                        }
+                    }
+                }
+                tasks.add(2, Task("Cellphone", "You used to call me on my", true, 12.0, 14.0))
+                taskAdapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Toast.makeText(
+                    applicationContext,
+                    "Failed to retrieve tasks: ${databaseError.message}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
+
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
